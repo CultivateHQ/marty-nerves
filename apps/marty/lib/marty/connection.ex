ed7bfda73c03735a1d48657fd35b7794954d49e8
Paul Wilson <paulanthonywilson@gmail.com>
2017-12-29 12:55:26 +0000

    Lifelike behaviours on connect

--- apps/marty/lib/marty/connection.ex
+++ apps/marty/lib/marty/connection.ex
@@ -1,12 +1,16 @@
 defmodule Marty.Connection do
   use GenServer
   alias Marty.Commands
+  require Logger
+
 
   @name __MODULE__
 
   @marty_ip {10, 0, 0, 49}
   @marty_port 24
   @connect_timeout 5_000
+  @retry_interval 2_000
+
 
   def start_link(_) do
     GenServer.start_link(__MODULE__, %{}, name: @name)
@@ -23,13 +27,24 @@ defmodule Marty.Connection do
   end
 
   def handle_info(:connect, s) do
-    {:ok, sock} = :gen_tcp.connect(@marty_ip, @marty_port, [], @connect_timeout)
-    :ok = :gen_tcp.send(sock, Commands.enable_safeties())
-    {:noreply, %{s | sock: sock}}
+    case :gen_tcp.connect(@marty_ip, @marty_port, [], @connect_timeout) do
+      {:ok, sock} ->
+        Logger.debug "Connected to Marty"
+        :ok = :gen_tcp.send(sock, Commands.enable_safeties())
+        :ok = :gen_tcp.send(sock, Commands.lifelike_behaviours(true))
+        {:noreply, %{s | sock: sock}}
+      err ->
+        Logger.debug fn -> "Failed to connect to Marty: #{inspect(err)}" end
+        Process.send_after(self(), :connect, @retry_interval)
+        {:noreply, s}
+    end
   end
 
+  def handle_call({:send_command, _}, _, s = %{sock: nil}) do
+    {:reply, {:error, :not_connected}, s}
+  end
 
-  def handle_call({:send_command, command}, _, s = %{sock: sock}) when not is_nil(sock) do
+  def handle_call({:send_command, command}, _, s = %{sock: sock}) do
     result = :gen_tcp.send(sock, command)
     {:reply, result, s}
   end
